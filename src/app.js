5facd10b6aabda3baafcd5850246c449c86fd351
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-08-20 12:24:29 +0530

    4.route and req

--- src/app.js
+++ src/app.js
@@ -1,52 +1,14 @@
 const express = require('express');
-
 const app = express();
 
-
-// OUTE ORDER MATTERS
-// app.use("/user",(req,res) => {
-//     res.send('haaaaaaaaaaaaaaaaaaaaa!');
-// })
-
-// this will match all the http methods API calls with user path
-app.get("/user", (req, res) => {
+app.get("/user/:userid", (req, res) => {
+    console.log(req.params);
     res.send({
         name: "Sourabh",
         age: 22
     });
 });
 
-
-app.post("/user", (req, res) => {
-    res.send({
-        name: "Sourabh",
-        age: 22,
-        httpMethod: "POST"
-    });
-});
-
-app.delete("/user", (req, res) => {
-    res.send({
-        name: "Sourabh",
-        age: 22,
-        httpMethod: "DELETE"
-    });
-});
-
-// this will match all the http methods API calls with user path
-app.use("/test", (req, res) => {
-    res.send('test from test!');
-});
-
-
-
-
-
-// app.use("/", (req, res) => {
-//     res.send('Welcome /!');
-// });
-
-
 app.listen(3000, () => {
-  console.log('Server is running on localhost:3000');
+    console.log("Server is running on port 3000");
 });
