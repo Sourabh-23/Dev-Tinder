116ed7c71bd3d5d0a78c8826fb0f10c5b77a4091
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-08-20 12:05:12 +0530

    EXPLORING crud

--- src/app.js
+++ src/app.js
@@ -2,19 +2,51 @@ const express = require('express');
 
 const app = express();
 
+
+// OUTE ORDER MATTERS
+// app.use("/user",(req,res) => {
+//     res.send('haaaaaaaaaaaaaaaaaaaaa!');
+// })
+
+// this will match all the http methods API calls with user path
+app.get("/user", (req, res) => {
+    res.send({
+        name: "Sourabh",
+        age: 22
+    });
+});
+
+
+app.post("/user", (req, res) => {
+    res.send({
+        name: "Sourabh",
+        age: 22,
+        httpMethod: "POST"
+    });
+});
+
+app.delete("/user", (req, res) => {
+    res.send({
+        name: "Sourabh",
+        age: 22,
+        httpMethod: "DELETE"
+    });
+});
+
+// this will match all the http methods API calls with user path
 app.use("/test", (req, res) => {
-    res.send('test from Express server!');
+    res.send('test from test!');
 });
 
 
-app.use("/hello", (req, res) => {
-    res.send('Hello World!g');
-});
 
 
-app.use("/", (req, res) => {
-    res.send('Welcome to the Express !');
-});
+
+// app.use("/", (req, res) => {
+//     res.send('Welcome /!');
+// });
+
+
 app.listen(3000, () => {
   console.log('Server is running on localhost:3000');
 });
