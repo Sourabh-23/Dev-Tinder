16ddc35839a38e05a05475eb334166a594165d0f
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-08-29 14:40:31 +0530

    Episode-08 | Data base leve validaton

--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,50 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
     },
     lastName: {
-        type: String
+        type: String,
+        required: true
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,      
+        lowercase: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
+        type: String,
+        validate(value) {
+            if(!["male", "female","others"].includes(value)) { 
+                throw new Error("Invalid gender");
+            }
+        }
+    },
+    photourl: {
         type: String
+    },
+    about: {
+        type: String,
+        default: "This is default about the user"
+    },
+    skills: {
+        type: [String]
     }
+}, {
+    timestamps: true    // ← ye SECOND argument hai, schema fields ke bahar
 });
 
 module.exports = mongoose.model('User', userSchema);
-           
-    
